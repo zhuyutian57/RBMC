348c633c940386521b988b2e50767789c9c29965
zhuyt <zhuyutian57@gmail.com>
2025-03-20 06:19:29 +0000

    x.py for debug

---
+++ x.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+# Script for debug
+
+def mirv(file, mirv_args):
+  assert(os.path.exists(file))
+  cmd = ["cargo", "run", "--bin", "mirv", file] + mirv_args
+  os.system(" ".join(cmd))
+  crate = os.path.splitext(os.path.basename(file))[0]
+  if os.path.exists(crate): os.system(f'rm {crate}')
+
+if __name__ == "__main__":
+  import argparse
+  import os
+  import sys
+
+  parser = argparse.ArgumentParser()
+  parser.add_argument(
+    "--build",
+    action="store_true",
+    help="Run `cargo build`")
+  parser.add_argument(
+    "--clean",
+    action="store_true",
+    help="Run `cargo clean`")
+  parser.add_argument(
+    "-f", "--file",
+    type=str,
+    help="Run `cargo run --bin mirv *.rs`. The generated binary file will be deleted")
+  parser.add_argument(
+    "--install",
+    action="store_true",
+    help="Run `cargo install --path .`")
+  parser.add_argument(
+    "--uninstall",
+    action="store_true",
+    help="Run `cargo uinsntall mirv`")
+
+  args, mirv_args = parser.parse_known_args()
+
+  if args.build:
+    os.system("cargo build")
+  elif args.clean:
+    os.system("cargo clean")
+  elif args.file is not None:
+    mirv(args.file, mirv_args)
+  elif args.install:
+    os.system("cargo install --path .")
+  elif args.uninstall:
+    os.system("cargo uninstall mirv")
+  else:
+    parser.print_help(sys.stdout)
+
+
+
+  
