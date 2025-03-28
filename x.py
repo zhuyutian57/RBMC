29532159a75bdacc5355fb152491426fab146cc8
zhuyt <zhuyutian57@gmail.com>
2025-03-28 09:52:44 +0000

    remove offline

--- x.py
+++ x.py
@@ -49,7 +49,7 @@ if __name__ == "__main__":
     mirv(args.file, mirv_args)
   elif args.install:
     os.system("cd ./library && cargo build --release")
-    os.system("cargo install --path . --offline")
+    os.system("cargo install --path .")
   elif args.uninstall:
     os.system("cargo uninstall mirv")
   else:
