840bd2dc2fb243855239bf78bffdec6287a8c9df
zhuyt <zhuyutian57@gmail.com>
2025-04-14 12:18:07 +0000

    reconstruct the project

--- src/bin/cargo-rbmc.rs
+++ src/bin/cargo-rbmc.rs
@@ -1,58 +1,62 @@
-#![feature(rustc_private)]
+// #![feature(rustc_private)]
 
-extern crate rustc_driver;
-extern crate rustc_interface;
-extern crate rustc_middle;
-extern crate rustc_smir;
-extern crate stable_mir;
+// extern crate rustc_driver;
+// extern crate rustc_interface;
+// extern crate rustc_middle;
+// extern crate rustc_smir;
+// extern crate stable_mir;
 
-use std::process::Command;
+// use std::process::Command;
 
-use rust_bmc::config::cli;
+// fn parse_mirv_flags() -> String {
+//     let mut args = std::env::args().into_iter().collect::<Vec<_>>();
+//     let idx =
+//         args.iter().enumerate().find_map(|(i, x)| if x == "--rbmc-args" { Some(i) } else { None });
+//     match idx {
+//         Some(i) => args.split_off(i).join(" "),
+//         None => "".to_string(),
+//     }
+// }
 
-fn parse_mirv_flags() -> String {
-    let mut args = std::env::args().into_iter().collect::<Vec<_>>();
-    let idx =
-        args.iter().enumerate().find_map(|(i, x)| if x == "--rbmc-args" { Some(i) } else { None });
-    match idx {
-        Some(i) => args.split_off(i).join(" "),
-        None => "".to_string(),
-    }
-}
+// fn main() {
+//     let metadata_cmd = cargo_metadata::MetadataCommand::new();
+//     let metadata = match metadata_cmd.exec() {
+//         Ok(m) => m,
+//         _ => panic!("Fail to get the metadata of the current project"),
+//     };
+
+//     // We only check the current crate
+//     if let Some(root) = metadata.root_package() {
+//         for target in &root.targets {
+//             if target.is_test() {
+//                 continue;
+//             }
+//             println!("Target: {}", target.name);
+//             let mut cmd = Command::new("cargo");
+//             cmd
+//                 // Set the crate being verified
+//                 .env(cli::RBMC_CRATE, target.name.as_str())
+//                 // Mirv arguments
+//                 .env(cli::RBMC_FLAGS, parse_mirv_flags())
+//                 // Wrap the rustc with rbmc
+//                 .env("RUSTC_WRAPPER", "rbmc")
+//                 // No need to compile the whole project
+//                 .arg("build");
+
+//             let exit_status =
+//                 cmd.spawn().expect("could not run cargo").wait().expect("failed to wait for cargo");
+
+//             if !exit_status.success() {
+//                 std::process::exit(exit_status.code().unwrap_or(-1))
+//             }
+//         }
+//     } else {
+//         panic!("Not support lib yet");
+//     }
+// }
+
+/// Cargo rbmc
 
 fn main() {
-    let metadata_cmd = cargo_metadata::MetadataCommand::new();
-    let metadata = match metadata_cmd.exec() {
-        Ok(m) => m,
-        _ => panic!("Fail to get the metadata of the current project"),
-    };
 
-    // We only check the current crate
-    if let Some(root) = metadata.root_package() {
-        for target in &root.targets {
-            if target.is_test() {
-                continue;
-            }
-            println!("Target: {}", target.name);
-            let mut cmd = Command::new("cargo");
-            cmd
-                // Set the crate being verified
-                .env(cli::RBMC_CRATE, target.name.as_str())
-                // Mirv arguments
-                .env(cli::RBMC_FLAGS, parse_mirv_flags())
-                // Wrap the rustc with rbmc
-                .env("RUSTC_WRAPPER", "rbmc")
-                // No need to compile the whole project
-                .arg("build");
-
-            let exit_status =
-                cmd.spawn().expect("could not run cargo").wait().expect("failed to wait for cargo");
-
-            if !exit_status.success() {
-                std::process::exit(exit_status.code().unwrap_or(-1))
-            }
-        }
-    } else {
-        panic!("Not support lib yet");
-    }
 }
--- src/bin/rbmc.rs
+++ src/bin/rbmc.rs
@@ -1,40 +1,6 @@
-#![feature(rustc_private)]
 
-extern crate rustc_driver;
-extern crate rustc_interface;
-extern crate rustc_middle;
-#[macro_use]
-extern crate rustc_smir;
-extern crate stable_mir;
+/// Set flags and run rbmc-driver
 
-use rustc_smir::{run, rustc_internal};
-use stable_mir::CompilerError;
-
-use std::ops::ControlFlow;
-use std::process::ExitCode;
-
-use rust_bmc::bmc::bmc::Bmc;
-use rust_bmc::config::cli::{self, Cli};
-use rust_bmc::config::config::Config;
-
-fn main() -> ExitCode {
-    let cli = Cli::new();
-    match run!(cli.rustc_args(), || start_bmc(cli)) {
-        Ok(_) | Err(CompilerError::Skipped) | Err(CompilerError::Interrupted(_)) => {
-            ExitCode::SUCCESS
-        }
-        _ => ExitCode::FAILURE,
-    }
-}
-
-fn start_bmc(cli: Cli) -> ControlFlow<()> {
-    // Verify when the current crate is variable.
-    let local_crate = stable_mir::local_crate().name;
-    if !cli.cur_crate().is_empty()
-        || matches!(std::env::var(cli::RBMC_CRATE), Ok(x) if local_crate == x)
-    {
-        Bmc::new(&Config::new(cli)).do_bmc();
-    }
-
-    ControlFlow::Continue(())
+fn main() {
+    
 }
